4e3259f6947ddd3a4973ff3f3bc1618a9941a451
VarunVB <varunvb3@gmail.com>
2015-10-15 23:13:52 +0530

    signal handling

--- chap10/actionhandler.c
+++ chap10/actionhandler.c
@@ -1,4 +1,13 @@
 #include <stdio.h>
+#include <signal.h>
+#include <stdlib.h>
+
+void godown (int sig) 
+{
+
+  puts("goodbye! don't you play with the window again, idiot\n"); 
+  exit(1);
+}
 
 int catch_signal(int sig, void(*handler)(int)){
 
@@ -7,4 +16,17 @@ int catch_signal(int sig, void(*handler)(int)){
    sigemptyset(&action.sa_mask);
    action.sa_flags = 0;
    return sigaction (sig, &action, NULL);
+}
+
+int main () 
+{
+    if (catch_signal(SIGWINCH, godown) == -1){
+	fprintf(stderr, "can't map the handler");
+	exit(2);
+     }
+     char name[30];
+     printf ("enter your name: ");
+     fgets(name, 30, stdin);
+     printf("Hello %s\n", name);
+     return 0;
 }
